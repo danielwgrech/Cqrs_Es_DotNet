4f6351a9e3deadd37d1d42ed98499a9fedf5c8fd
Mirko Zagami <mirko.zagami@gmail.com>
2018-12-10 00:17:08 +0100

    Initial commit

---
+++ CommandsQueries/Grade.cs
@@ -0,0 +1,7 @@
+namespace CommandsQueries
+{
+    public class Grade
+    {
+        
+    }
+}
---
+++ CommandsQueries/StudentTest.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace CommandsQueries
+{
+    [TestFixture]
+    public class StudentTest
+    {
+        
+    }
+}
